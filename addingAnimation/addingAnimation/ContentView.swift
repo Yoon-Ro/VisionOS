26bbf0ef03dab0acaef27083680a9e1401ab8183
Yoon Ro <87830420+Yoon-Ro@users.noreply.github.com>
2023-12-29 17:45:47 -0800

    addingAnimation

--- addingAnimation/addingAnimation/ContentView.swift
+++ addingAnimation/addingAnimation/ContentView.swift
@@ -9,10 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
 
+    @State var isAnimated: Bool = false
 
 
     var body: some View {
-        Text("Asd")
+        NavigationStack{
+            VStack{
+                Button("Button")                {
+//                    isAnimated.toggle()
+//withAnimation(.default){isAnimated.toggle()}
+//withAnimation(Animation.default.delay(2.0)){isAnimated.toggle()}
+withAnimation(Animation.default.repeatCount(10, autoreverses: true)){isAnimated.toggle()}
+                    
+                }
+                
+                RoundedRectangle(cornerRadius: isAnimated ? 25.0 : 50)
+                    .fill(isAnimated ? Color.green : Color.orange)
+                    .frame(width: isAnimated ? 100 : 150, height: 100)
+                    .offset(y: isAnimated ? 150 : 0)
+                    .rotationEffect(Angle(degrees: isAnimated ? 180 : 0))
+                    .frame(depth: isAnimated ? 200 : 0)
+//                    .animation(Animation
+//                        .default
+//                        .repeatCount(5, autoreverses: true))
+                
+                
+            }
+        }
     }
 }
 
