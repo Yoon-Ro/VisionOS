8bab144f3c331d9e4982b49ef6942d962855372e
Yoon Ro <87830420+Yoon-Ro@users.noreply.github.com>
2024-01-06 22:16:36 -0800

    Learning Alert

---
+++ Alert/Alert/AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertApp.swift
+//  Alert
+//
+//  Created by Yoon Ro on 2024-01-06.
+//
+
+import SwiftUI
+
+@main
+struct AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alert/Alert/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Alert
+//
+//  Created by Yoon Ro on 2024-01-06.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
